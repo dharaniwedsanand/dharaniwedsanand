79f1c12c7fb4187fad023335a3dc54ee8ce8295d
Manikandan Mohan <mkfeb12@gmail.com>
2018-10-24 00:47:39 +0530

    Added new timer with glow

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-(() => {
-    document.addEventListener("DOMContentLoaded", function () {
-        let currentYear = new Date().getFullYear();
-        let targetDate = new Date(2018, 11, 12, 09, 30, 0);
-        let onStart = () => {
-            document.querySelectorAll('.countdown-item').forEach(item => item.classList.add('show'))
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
         }
-        let onTick = ({ days, hours, minutes, seconds }) => {
-            document.querySelector('.countdown-item.days').innerHTML = days;
-            document.querySelector('.countdown-item.hours').innerHTML = hours;
-            document.querySelector('.countdown-item.minutes').innerHTML = minutes;
-            document.querySelector('.countdown-item.seconds').innerHTML = seconds;
-        };
-        let options = new LsCountdownOptions({ targetDate, onStart, onTick });
-        let countdown = new LsCountdown(options);
-
-        countdown.start();
-        window.mycountdown = countdown;
-    });
-})();
+    }
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+var deadline = new Date(2018, 11, 12, 9, 0, 0);
+initializeClock('clockdiv', deadline);
